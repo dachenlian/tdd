8e9371c3dba90dee66bc7363d79b8087100e9d58
richl <cclemon.lian@gmail.com>
2018-09-23 09:30:18 +0800

    new URL, new and template to display lists

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -6,9 +6,11 @@ from .models import Item
 
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/')
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'lists/home.html')
 
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'lists/home.html', {'items': items})
+    return render(request, 'lists/list.html', {'items': items})
